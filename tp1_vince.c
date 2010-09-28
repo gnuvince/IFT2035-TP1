f298300d869ca3228320520ff9aab500fdc5a6c1
Vincent Foley <vfoley@gmail.com>
2010-09-28 19:26:46 -0400

    Removed commented code for push

--- tp1_vince.c
+++ tp1_vince.c
@@ -53,6 +53,37 @@ struct Expr {
 
 enum TokenizerState { tok_normal, tok_number, tok_operator };
 
+
+
+/***
+ * List manipulation functions.
+ ***/
+struct List *ListNew() {
+    struct List *list = malloc(sizeof(struct List));
+    list->head = NULL;
+    return list;
+}
+
+void ListFree(struct List *list) {
+    /* Free every node in the list. */
+    while (list->head != NULL) {
+        struct Node *next = list->head->next;
+        free(list->head);
+        list->head = next;
+    }
+
+    /* Free the actual list pointer. */
+    free(list);
+}
+
+void ListAdd(struct Token *t, struct List *list) {
+    struct Node *new = malloc(sizeof(struct Node));
+    new->value = *t;
+    new->next = list->head;
+    list->head = new;
+}
+
+
 int isspace(int c) {
     return c == ' ' || c == '\t' || c == '\n';
 }
@@ -72,86 +103,28 @@ int digit_to_int(int c) {
     return c - '0';
 }
 
-
-struct List *ListNew() {
-    struct List *list = malloc(sizeof(struct List));
-    list->head = NULL;
-    return list;
-}
-
-void ListFree(struct List *list) {
-    while (list->head != NULL) {
-        struct Node *next = list->head->next;
-        free(list->head);
-        list->head = next;
-    }
-    free(list);
-}
-
-void ListAdd(struct Token *t, struct List *list) {
-    struct Node *new = malloc(sizeof(struct Node));
-    new->value = *t;
-    new->next = list->head;
-    list->head = new;
-}
-
-/*
-enum ErrorCode Tokenize(struct List *tokens) {
-    int c;
-    int currentNumber;
-    enum TokenizerState state = tok_normal;
-
-    while (1) {
-        c = getchar();
-
-        switch (state) {
-        case tok_normal:
-            if (isdigit(c)) {
-                currentNumber = digit_to_int(c);
-                state = tok_number;
-            }
-            else if (isoperator(c))
-                state = tok_operator;
-            break;
-
-        case tok_number:
-            if (isdigit(c)) {
-                currentNumber = currentNumber * 10 + digit_to_int(c);
-            }
-            else {
-                printf("%d\n", currentNumber);
-                state = tok_normal;
-            }
-            break;
-
-        case tok_operator:
-            if (isspace(c)) {
-                state = tok_normal;
-            }
-            break;
-
-        default:
-            break;
-        }
-    }
-}
-*/
-
 int main(void) {
     struct List *l = ListNew();
     struct Node *n;
     int i;
 
-    for (i = 10; i > 0; --i) {
         struct Token t;
+    for (i = 5; i > 0; --i) {
         t.type = num;
         t.value.number = i;
         ListAdd(&t, l);
     }
 
+    t.type = op;
+    t.value.operator = op_mul;
+    ListAdd(&t, l);
+
 
     for (n = l->head; n != NULL; n = n->next) {
-        printf("%d\n", (*n).value.value.number);
+        if ((*n).value.type == num)
+            printf("[NUM] %d\n", (*n).value.value.number);
+        else
+            printf("[OP ] %d\n", (*n).value.value.operator);
     }
 
     ListFree(l);
