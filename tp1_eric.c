523354b8afd5a531a83c0e599db58225b9c41718
Eric <thiveri@gmail.com>
2010-09-28 19:07:26 -0400

    free les mallocs de la liste

--- tp1_eric.c
+++ tp1_eric.c
@@ -79,99 +79,107 @@ void printList(struct List *L) {
 
 struct List * getList() {
 	// On construit une liste a mano representant l'expression:
-		// 31 4 - 8 5 * + 67 /
-		// ==> [(/, 67, +, *, 5, 8, -, 4, 31)]
+	// 31 4 - 8 5 * + 67 /
+	// ==> [(/, 67, +, *, 5, 8, -, 4, 31)]
 
-		struct List *L, *last;
+	struct List *L, *last;
 
-		struct Token *token = malloc(sizeof(struct Token));
-		token->type = op;
-		token->value.operator = op_div;
+	struct Token *token = malloc(sizeof(struct Token));
+	token->type = op;
+	token->value.operator = op_div;
 
-		struct List *node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		L = node;
-		last = node;
+	struct List *node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	L = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = num;
-		token->value.number = 67;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = num;
+	token->value.number = 67;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = op;
-		token->value.number = op_add;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = op;
+	token->value.number = op_add;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = op;
-		token->value.number = op_mul;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = op;
+	token->value.number = op_mul;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = num;
-		token->value.number = 5;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = num;
+	token->value.number = 5;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = num;
-		token->value.number = 8;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = num;
+	token->value.number = 8;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = op;
-		token->value.number = op_sub;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = op;
+	token->value.number = op_sub;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = num;
-		token->value.number = 4;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = num;
+	token->value.number = 4;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		token = malloc(sizeof(struct Token));
-		token->type = num;
-		token->value.number = 31;
-		node = malloc(sizeof(struct List));
-		node->value = *token;
-		node->next = NULL;
-		last->next = node;
-		last = node;
+	token->type = num;
+	token->value.number = 31;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
 
-		return L;
+	free(token);
+	return L;
+}
+
+void freeList(struct List *L) {
+	struct List *elt, *next;
+
+	elt = L;
+
+	while (elt != NULL) {
+		next = elt->next;
+		free(elt);
+		elt = next;
+	}
 }
 
 int main() {
+	struct List *L;
 
-	printList(getList());
+	L = getList();
+	printList(L);
+	freeList(L);
 
 	return 0;
 }
