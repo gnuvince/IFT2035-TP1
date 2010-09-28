0d24e0f9ca7e59472de172a4f925cf544fb8bab9
Eric Thivierge <thiveri@gmail.com>
2010-09-28 15:12:30 -0400

    modifs mineures sur les types de base. Liste a mano

--- tp1_eric.c
+++ tp1_eric.c
@@ -2,8 +2,11 @@
  * tp1.c
  *
  *  Created on: Sep 27, 2010
- *      Author: eric
+ *      Author: eric & vince
  */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 enum Operator {
 	op_add, op_sub, op_mul, op_div
@@ -24,17 +27,110 @@ struct Token {
 };
 
 struct List {
-	Token value;
-	List *next;
+	struct Token value;
+	struct List *next;
 };
 
 struct Expr {
-	enum { num, expr } type;
+	enum { operand, expr } type;
 	union {
 		Number number;
 		struct BinaryOperator {
-			Operator operator;
-			struct Expr left, right;
+			enum Operator operator;
+			struct Expr *left;
+			struct Expr *right;
 		} expression;
 	} value;
 };
+
+int main() {
+	// On construit une liste a mano representant l'expression:
+	// 31 4 - 8 5 * + 67 /
+	// ==> [(/, 67, +, *, 5, 8, -, 4, 31)]
+
+	struct List *L, *last;
+
+	struct Token *token = malloc(sizeof(struct Token));
+	token->type = op;
+	token->value.operator = op_div;
+
+	struct List *node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	L = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = num;
+	token->value.number = 67;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = op;
+	token->value.number = op_add;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = op;
+	token->value.number = op_mul;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = num;
+	token->value.number = 5;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = num;
+	token->value.number = 8;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = op;
+	token->value.number = op_sub;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = num;
+	token->value.number = 4;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	token = malloc(sizeof(struct Token));
+	token->type = num;
+	token->value.number = 31;
+	node = malloc(sizeof(struct List));
+	node->value = *token;
+	node->next = NULL;
+	last->next = node;
+	last = node;
+
+	return 0;
+}
